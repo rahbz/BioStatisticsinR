00340817894e88d7860ba87db1a43cb96643f2e9
Rahul Pisupati <rahulpisupati@Rahuls-iMac.local>
2017-07-25 17:18:03 +0200

    workingvetmed

--- scripts/bayesian_likelihood_simulations.R
+++ scripts/bayesian_likelihood_simulations.R
@@ -3,14 +3,8 @@
 #
 
 
-## Simulations, with specific number of cytosines and error rates
+## Simulations, with binomial probability distributions
 #
-
-error.rates <- 0.02  ### Conversion of 98% 
-
-num.cytosines <- 4
-
-
 likelihoodFunction <- function(p, mC, tC){
   a <- choose(n = tC,k = mC)
   b <- p ^ mC
@@ -18,28 +12,25 @@ likelihoodFunction <- function(p, mC, tC){
   return(a * b * c)
 }
 
-likelihoodFunction(5, 2, 0.99999)
-
 getPoints <- function(mC, tC){
   x <- seq(0, 1, length.out = 200)
   y <- sapply(x, FUN = function(x){likelihoodFunction(p = x, mC = mC, tC = tC)})
   return(cbind(x,y))
 }
 
-
 plot(getPoints(1,5))   ### 2/5
 points(getPoints(4,10), col="blue")   ## 4/10
 points(getPoints(40,100), col="red")   ## 4/10
 points(getPoints(1,3), col = "orange")
 
-
-
 optimise(interval = c(0,1),f = likelihoodFunction, mC = 1, tC = 3, maximum = T, tol = 0.001)
 
 
 
-
-
+## Simulations with the methylation status of reads with given cytosines
+#
+error.rates <- 0.02  ### Conversion of 98% 
+num.cytosines <- 4
 
 
 
